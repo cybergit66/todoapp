a0085149fc05e6683325f0bb9f6d3cbf6839a57a
Terence <terence.highsmith@gmail.com>
2016-11-29 11:50:06 -0600

    add test for handleaddtodo

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -10,4 +10,19 @@ describe('TodoApp', () => {
   it('TodoApp component should exist', () => {
     expect(TodoApp).toExist();
   });
+    
+    it('should add todo to the todos state on handleAddTodo', () => {
+        // create new todo to add
+        var todoText = 'add new todo';
+        // create instance of TodoApp component
+        var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+        
+        // empty the todos state                                 
+        todoApp.setState({todos: []});
+        // add new todo to todos state
+        todoApp.handleAddTodo(todoText);
+        
+        // test state for added todo
+        expect(todoApp.state.todos[0].text).toBe(todoText);
+    });
 });
