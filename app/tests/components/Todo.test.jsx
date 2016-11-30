ac15c7be9e153174ec9364d4447b870d5346499f
Terence <terence.highsmith@gmail.com>
2016-11-30 10:33:35 -0600

    added toggle support and testing

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -10,4 +10,21 @@ describe('Todo', () => {
   it('Todo component should exist', () => {
     expect(Todo).toExist();
   });
+    
+    it('should call onToggle prop with id on click', () => {
+        var todoData = {
+            id: 199,
+            text: 'Write todo.test.jsx',
+            completed: true
+        };
+        var spy = expect.createSpy();
+        var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}/>);
+        
+        //Access the jquery selector
+        var $el = $(ReactDOM.findDOMNode(todo));
+        //pull out the element and simulate click event
+        TestUtils.Simulate.click($el[0]);
+        //write assertion that checks spy was called successfully with id: 199
+        expect(spy).toHaveBeenCalledWith(199);
+    })
 });
