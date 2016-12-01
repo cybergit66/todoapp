a3fe57ad3a191127f3278a9170833eba9aa7052a
Terence Highsmith <thighsmi@mssda16050503.msfc.nasa.gov>
2016-12-01 16:14:28 -0600

    added search functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -19,5 +19,39 @@ module.exports = {
         
         // ternary operation - if todos is an array, then return todos, else return empty array
         return $.isArray(todos) ? todos : [];
+    },
+    filterTodos: function(todos, showCompleted, searchText){
+        var filteredTodos = todos;
+        
+        // filter by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+        
+        // filter by searchText
+        filteredTodos = filteredTodos.filter((todo) => {
+            var str = todo.text.toLowerCase();
+            return searchText.length === 0 || str.indexOf(searchText) > -1;
+        });
+            // use filter method on filtered todos
+                // check for search text
+                    // if no search text return every todo item
+                    // if search text exist then 
+                        // convert to lowercase, e.g. text.toLowerCase()
+                        // check todo using IndexOf
+        
+        
+        // sort todos with non-completed first
+        filteredTodos.sort((a,b) => {
+           if (!a.completed && b.completed) {
+               return -1
+           } else if (a.completed && !b.completed){
+               return 1
+           } else {
+               return 0;
+           }
+        });
+        
+        return filteredTodos;
     }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -52,4 +52,47 @@ describe('TodoAPI', () => {
             expect(actualTodos).toEqual(todos);
         })
     });
+    
+    describe('filterTodos', () => {
+        var todos = [{
+            id: 1,
+            text: 'text',
+            completed: true
+        },
+                    {
+            id: 2,
+            text: 'some more text',
+            completed: false
+        },
+                    {
+            id: 3,
+            text: 'some last text',
+            completed: true
+        }];
+        
+        it('should return 3 items if show completed is true', () =>{
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '')
+            expect (filteredTodos.length).toBe(3);
+        });
+        
+        it('should return 1 items if show completed is false', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+            expect(filteredTodos.length).toBe(1);
+        });
+        
+        it('should sort by completed status', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+        
+        it('should filter todos by search text', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+            expect(filteredTodos.length).toBe(2);
+        });
+        
+        it('should return all todos if search text is empty', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
+        });
+    });
 });
